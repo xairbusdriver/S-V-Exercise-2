0338d3d30173e8558d5b87fb821c44a15a44fc1e
James Chaffin <xairbusdriver@james-imac.local>
2018-04-06 14:44:09 -0500

    Added a reverse txt function to a branched project

--- StackViewDemoExercise/ViewController.swift
+++ StackViewDemoExercise/ViewController.swift
@@ -15,11 +15,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print(message)
+		print(reverse(text: "stressed"))
     }
 
+	func reverse(text: String) -> String {
+		return String(text.reversed())
+	}
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+		
     }
 
 
